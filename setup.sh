c680af5b26f019f86a41309e3e2def0b6d5ff8d4
Adam Peryman <adam.peryman@gmail.com>
2018-01-10 14:54:55 +1300

    Still tinkering with shell permissions.

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ VIM_CONFIG_PATH=$home_dir/.vim_runtime
 # Backup Amix's my_configs.vim
 if [ -f $VIM_CONFIG_PATH/my_configs.vim ]; then
   echo "Beginning my_configs.vim backup.."
-  sudo -S mv $VIM_CONFIG_PATH/my_configs.vim $VIM_CONFIG_PATH/my_configs.vim.BAK
+  sudo mv $VIM_CONFIG_PATH/my_configs.vim $VIM_CONFIG_PATH/my_configs.vim.BAK
   echo "my_configs.vim backed up successfully.\n"
 else
   echo "my_configs.vim file not found, no backup necessary.\n"
