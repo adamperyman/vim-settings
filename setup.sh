05a716cb285718066dcca68ef2d5700ae10d4dd2
Adam Peryman <adam.peryman@gmail.com>
2018-01-10 16:06:12 +1300

    Remove sudo.

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ VIM_CONFIG_PATH=$home_dir/.vim_runtime
 # Backup Amix's my_configs.vim
 if [ -f $VIM_CONFIG_PATH/my_configs.vim ]; then
   echo "Beginning my_configs.vim backup.."
-  sudo mv $VIM_CONFIG_PATH/my_configs.vim $VIM_CONFIG_PATH/my_configs.vim.BAK
+  mv $VIM_CONFIG_PATH/my_configs.vim $VIM_CONFIG_PATH/my_configs.vim.BAK
   echo "my_configs.vim backed up successfully.\n"
 else
   echo "my_configs.vim file not found, no backup necessary.\n"
@@ -14,7 +14,7 @@ fi
 
 # Patch .vimrc
 echo "Beginning Adam's .vimrc patch.."
-sudo sh -c 'cat .vimrc > $VIM_CONFIG_PATH/my_configs.vim'
+bash -c 'cat .vimrc > $VIM_CONFIG_PATH/my_configs.vim'
 echo "Adam's .vimrc patch complete.\n"
 
 echo "Adam's Vim settings have been applied successfully. Have a wonderful day :)"
