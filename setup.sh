bc9066f5ad0bb51c9414b09fcb5ecadc6b81d838
Adam Peryman <adam.peryman@gmail.com>
2018-01-10 14:37:16 +1300

    Super cat.

--- setup.sh
+++ setup.sh
@@ -13,7 +13,7 @@ fi
 
 # Patch .vimrc
 echo "Beginning Adam's .vimrc patch.."
-cat .vimrc > $VIM_CONFIG_PATH/my_configs.vim
+sudo cat .vimrc > $VIM_CONFIG_PATH/my_configs.vim
 echo "Adam's .vimrc patch complete.\n"
 
 echo "Adam's Vim settings have been applied successfully. Have a wonderful day :)"
